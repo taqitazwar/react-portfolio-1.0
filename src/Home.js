140e3dbb18cd693fa8f97396cc5178d20a5f2c5c
Taqi Tazwar <189677955+taqitazwar@users.noreply.github.com>
2025-07-12 14:26:34 -0230

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -173,7 +173,7 @@ const Home = ({ setCurrentPage }) => {
             </button>
             
             <a 
-              href="https://drive.google.com/drive/folders/1YYXFVT4NEkgt9u2Wbbbsecysw43TXLIG?usp=sharing" 
+              href="https://drive.google.com/file/d/1VYZizFZx3KKn5EZoM_IORsFr4NFLyzSO/view?usp=sharing" 
               className="resume-btn interactive"
               target="_blank"
               rel="noopener noreferrer"
